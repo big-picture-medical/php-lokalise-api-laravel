4f057b62af5022640d00c8cf8187f65c7249b9e4
Ruben Funai <ruben@bigpicturemedical.com>
2021-09-17 16:59:16 +1000

    implement service provider

---
+++ src/LokaliseServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ARG2009\Lokalise;
+
+use Illuminate\Support\ServiceProvider;
+use Lokalise\LokaliseApiClient;
+
+class LokaliseServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(LokaliseApiClient::class, function ($app) {
+            return new LokaliseApiClient($app['config']['services.lokalise.key']);
+        });
+    }
+}
